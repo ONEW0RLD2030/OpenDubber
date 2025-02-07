cfcef9da767e4acb4a6f5b7f8fda4793361f52c1
Human <onemarket1world@gmail.com>
2025-02-07 06:19:51 +0200

    Update .bash

--- .bash
+++ .bash
@@ -2,3 +2,5 @@ pip install -r requirements.txt
    sudo apt install ffmpeg          # لتشغيل FFmpeg
    git clone https://github.com/MycroftAI/mimic3-voices  # أصوات Mimic3
 python src/app.py
+sudo apt update && sudo apt install ffmpeg  # لنظام Linux
+
